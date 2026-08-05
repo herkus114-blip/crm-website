175b48fa8b9a4146f95c00d0b5a32729261d5ac0
Herkus <herkus114@gmail.com>
2026-08-05 20:46:59 +0400

    Fix Base44 authentication server

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -1,14 +1,7 @@
-import { createClient } from '@base44/sdk';
-import { appParams } from '@/lib/app-params';
+import { createClient } from "@base44/sdk";
+import { appParams } from "@/lib/app-params";
 
-const { appId, token, functionsVersion, appBaseUrl } = appParams;
-
-//Create a client with authentication required
 export const base44 = createClient({
-  appId,
-  token,
-  functionsVersion,
-  serverUrl: '',
-  requiresAuth: false,
-  appBaseUrl
+  appId: appParams.appId,
+  token: appParams.token || undefined,
 });
